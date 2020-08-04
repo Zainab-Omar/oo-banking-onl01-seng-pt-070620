684a202eaf5883a4d29480551194d9c4190447f3
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-04 17:50:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
         sender.deposit (amount * -1)
         receiver.deposit (amount)
         @status="complete"
-        elsif @status="complete"
+        elsif @status=="complete"
         puts "Transaction was already excuted"
       else @status="rejected"
         "Transaction rejected. Please check your account balance."
