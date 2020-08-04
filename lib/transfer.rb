7556e29d88228be45c9962a328502d244ec2f704
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-04 17:49:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,8 @@ class Transfer
   end
   
   def execute_transaction
-      if valid?eposit (amount * -1)
+      if valid? && sender.balance > amount
+        sender.deposit (amount * -1)
         receiver.deposit (amount)
         @status="complete"
         elsif @status="complete"
